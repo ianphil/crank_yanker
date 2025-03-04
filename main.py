4f2d758b876abc5e14beddcb341c22e36dd9cdf0
Ian Philpot <ianphil@microsoft.com>
2025-03-04 17:31:27 -0500

    Hello name

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from crank-yanker!")
+import click
 
+@click.command()
+@click.option('--name', prompt='Your name', help='The name of the user')
+def greet(name):
+    """Simple program that greets the user."""
+    click.echo(f"Hello, {name}! Welcome to the Cycling Agent.")
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    greet()
